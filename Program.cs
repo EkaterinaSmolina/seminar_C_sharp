3445a66df12797f78976a9a3f88c21f95a314bfe
Smolina <katerina_kat2101@mail.ru>
2022-08-07 17:51:25 +1000

    example_1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("введите число:  ");;
+int number = Convert.ToInt32(Console.ReadLine());
+number *=number;
+Console.WriteLine("число в квадрате = " + number);
