4071cd34a420dbd22e65acb8d9e511bca72709c5
Smolina <katerina_kat2101@mail.ru>
2022-08-09 20:36:09 +1000

    зание_1

--- Program.cs
+++ Program.cs
@@ -1,28 +1,15 @@
-﻿//Console.Write("введите число:  ");
-//int number = Convert.ToInt32(Console.ReadLine());
-//number *=number;
-//Console.WriteLine("число в квадрате = " + number);
-
-
-// домашнее задание:
-// задача 1
-//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
-//a = 5; b = 7 -> max = 7
+﻿//a = 5; b = 7 -> max = 7
 //a = 2 b = 10 -> max = 10
 //a = -9 b = -3 -> max = -3
 
-Console.WriteLine("Введите первое число: ");
-int number_1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите второе число: ");
-int number_2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("input a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("input b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int num_1=a;
+int num_2=b;
 
-if (number_1 > number_2);
-    Console.WriteLine(" max =" + number_1);
-
-else (number_1 < number_2);
-    Console.WriteLine(" max =" + number_2);
-
-
-
-
+if (num_1>num_2)
+    Console.WriteLine("a=" + num_1 + "; b=" + num_2 + "->" + "max= " + num_1);
+        else Console.WriteLine("a=" + num_1 + "; b=" + num_2 + "->" + "max= " + num_2);   
 
