14dd551da1b6a1f173eb617cc325ddabaa42ca97
Smolina <katerina_kat2101@mail.ru>
2022-08-09 20:33:19 +1000

    задание_1

--- Program.cs
+++ Program.cs
@@ -1,4 +1,28 @@
-﻿Console.Write("введите число:  ");;
-int number = Convert.ToInt32(Console.ReadLine());
-number *=number;
-Console.WriteLine("число в квадрате = " + number);
+﻿//Console.Write("введите число:  ");
+//int number = Convert.ToInt32(Console.ReadLine());
+//number *=number;
+//Console.WriteLine("число в квадрате = " + number);
+
+
+// домашнее задание:
+// задача 1
+//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+//a = 5; b = 7 -> max = 7
+//a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3
+
+Console.WriteLine("Введите первое число: ");
+int number_1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int number_2 = Convert.ToInt32(Console.ReadLine());
+
+if (number_1 > number_2);
+    Console.WriteLine(" max =" + number_1);
+
+else (number_1 < number_2);
+    Console.WriteLine(" max =" + number_2);
+
+
+
+
+
