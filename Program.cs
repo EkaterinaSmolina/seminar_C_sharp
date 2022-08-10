530e8c9dc97e2deec9b396b64534ba21cdc55fbc
Smolina <katerina_kat2101@mail.ru>
2022-08-10 20:12:57 +1000

    задание_2

--- Program.cs
+++ Program.cs
@@ -2,14 +2,33 @@
 //a = 2 b = 10 -> max = 10
 //a = -9 b = -3 -> max = -3
 
+//Console.WriteLine("input a: ");
+//int a = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("input b: ");
+//int b = Convert.ToInt32(Console.ReadLine());
+//int num_1=a;
+//int num_2=b;
+
+//if (num_1>num_2)
+//    Console.WriteLine("a=" + num_1 + "; b=" + num_2 + "->" + "max= " + num_1);
+//        else Console.WriteLine("a=" + num_1 + "; b=" + num_2 + "->" + "max= " + num_2);   
+
+
+
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+
 Console.WriteLine("input a: ");
 int a = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("input b: ");
 int b = Convert.ToInt32(Console.ReadLine());
-int num_1=a;
-int num_2=b;
+Console.WriteLine("input c: ");
+int c = Convert.ToInt32(Console.ReadLine());
 
-if (num_1>num_2)
-    Console.WriteLine("a=" + num_1 + "; b=" + num_2 + "->" + "max= " + num_1);
-        else Console.WriteLine("a=" + num_1 + "; b=" + num_2 + "->" + "max= " + num_2);   
-
+ if (a > b && a > c)
+ Console.WriteLine("a=" + a + "; b=" + b + "; с= "+ c + "->" + "max= " + a); 
+        else if (b > c)
+        Console.WriteLine("a=" + a + "; b=" + b + "; с= "+ c + "->" + "max= " + b); 
+                else
+                    Console.WriteLine("a=" + a + "; b=" + b + "; с= "+ c + "->" + "max= "+ c);
